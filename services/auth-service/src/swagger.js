968877b2d8075db354aa5b99393fa3ce0c8eeabe
Enes Buyuk <contact@enesbuyuk.com>
2025-02-12 07:15:10 +0300

    refactor: update server URLs in swagger files for each service

--- services/auth-service/src/swagger.js
+++ services/auth-service/src/swagger.js
@@ -14,7 +14,7 @@ const options = {
         },
         servers: [
             {
-                url: `http://localhost:${process.env.PORT}`,
+                url: `http://localhost:${process.env.PORT}/auth`,
                 description: "Docker container for this service",
             },
         ],
